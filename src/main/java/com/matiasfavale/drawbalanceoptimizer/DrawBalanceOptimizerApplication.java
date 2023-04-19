458bfdb960fb75ac303b87628ddcb10b0fe6ad17
Matias Alejandro Favale <mafavale@users.noreply.github.com>
2023-04-19 12:45:52 -0300

    feature: Initial project creation.

---
+++ src/main/java/com/matiasfavale/drawbalanceoptimizer/DrawBalanceOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.matiasfavale.drawbalanceoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawBalanceOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawBalanceOptimizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matiasfavale/drawbalanceoptimizer/DrawBalanceOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasfavale.drawbalanceoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawBalanceOptimizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
